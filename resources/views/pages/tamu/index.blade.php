f0c5300caf0195107131f4faec9247ce575a6ddd
Aryo07 <arek.yogya55@gmail.com>
2023-12-01 14:20:47 +0700

    fix/sorting data tamu

--- resources/views/pages/tamu/index.blade.php
+++ resources/views/pages/tamu/index.blade.php
@@ -35,7 +35,7 @@
                             <th>Aksi</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    {{-- <tbody>
                         @foreach ($tamulist as $key => $data)
                             <tr>
                                 <td><span class="text-muted">
@@ -51,12 +51,12 @@
                                 <td>{{ $data->jam_masuk }}</td>
                                 <td id="jamKeluarField">{{ $data->jam_keluar }}</td>
                                 <td>{{ $data->identitas }}</td>
-                                @if($data->foto_identitas)
+                                @if ($data->foto_identitas)
                                     <td><img src="{{ Storage::url($data->foto_identitas) }}" alt="Foto Tamu" width="150"></td>
                                 @else
                                     <td class="text-danger fw-bold">Tidak ada foto</td>
                                 @endif
-                                @if($data->foto_tamu)
+                                @if ($data->foto_tamu)
                                     <td><img src="{{ Storage::url($data->foto_tamu) }}" alt="Foto Tamu" width="150"></td>
                                 @else
                                     <td class="text-danger fw-bold">Tidak ada foto</td>
@@ -80,7 +80,7 @@
                                 </td>
                             </tr>
                         @endforeach
-                    </tbody>
+                    </tbody> --}}
                 </table>
             </div>
         </div>
@@ -92,34 +92,160 @@
 <script>
     $(document).ready(function() {
         $('#tableTamu').DataTable({
-            "pageLength": 50
+            "pageLength": 50,
+            searching: true,
+            processing: true,
+            serverSide: true,
+            paging: true,
+            "order": [],
+            bAutoWidth: false,
+            ajax: {
+                url: '{{ route('getData') }}',
+                type: 'GET',
+            },
+            columns: [{
+                    data: null,
+                    render: function(data, type, row, meta) {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    },
+                },
+                {
+                    data: 'name',
+                    name: 'name'
+                },
+                {
+                    data: 'name_instansi',
+                    name: 'name_instansi'
+                },
+                {
+                    data: 'tipe_tamu',
+                    name: 'tipe_tamu'
+                },
+                {
+                    data: 'alamat',
+                    name: 'alamat'
+                },
+                {
+                    data: 'pertemuan',
+                    name: 'pertemuan'
+                },
+                {
+                    data: 'keperluan',
+                    name: 'keperluan'
+                },
+                {
+                    data: 'jam_masuk',
+                    name: 'jam_masuk'
+                },
+                {
+                    data: 'jam_keluar',
+                    name: 'jam_keluar'
+                },
+                {
+                    data: 'identitas',
+                    name: 'identitas'
+                },
+                {
+                    data: 'foto_identitas',
+                    name: 'foto_identitas',
+                    render: function(data, type, row, meta) {
+                        if (data) {
+                            var url = window.location.origin + '/storage/' + data.replace(
+                                'public/', '');
+                            return '<img src="' + url + '" alt="Foto Identitas" width="150" />';
+                        } else {
+                            return '<span class="text-danger">foto tidak tersedia</span>';
+                        }
+                    }
+                },
+                {
+                    data: 'foto_tamu',
+                    name: 'foto_tamu',
+                    render: function(data, type, row, meta) {
+                        if (data) {
+                            var url = window.location.origin + '/storage/' + data.replace(
+                                'public/', '');
+                            return '<img src="' + url + '" alt="Foto Tamu" width="150" />';
+                        } else {
+                            return '<span class="text-danger">foto tidak tersedia</span>';
+                        }
+                    }
+                },
+                {
+                    data: null,
+                    render: function(data, type, row, meta) {
+                        return '<div class="form-switch">' +
+                            '<input type="checkbox" class="statusSwitch" ' +
+                            (row.status_keluar == 'keluar' ? 'checked' : '') +
+                            ' data-tamu-id="' + row.id + '">' +
+                            '<label for="statusSwitch"></label>' +
+                            '</div>';
+                    }
+                },
+                {
+                    // button edit dan delete
+                    data: null,
+                    render: function(data, type, row, meta) {
+                        return '<a href="{{ url('tamu') }}/' + row.id +
+                            '/edit" class="btn btn-info btn-sm">Edit</a>' +
+                            '<button class="btn btn-danger btn-sm ms-1" data-bs-toggle="modal" data-bs-target="#deleteModal" data-tamu-id="' +
+                            row.id + '">Hapus</button>';
+                    }
+                },
+            ],
         });
     });
 
-    $(document).on('change', '.statusSwitch', function(event) {
-    event.preventDefault();
+    // $(document).on('change', '.statusSwitch', function(event) {
+    //     event.preventDefault();
 
-    var action = $(this).is(":checked") ? 'keluar' : 'aktif';
-    var tamuId = $(this).closest('td').data('tamu-id');
+    //     var action = $(this).is(":checked") ? 'keluar' : 'aktif';
+    //     var tamuId = $(this).closest('td').data('tamu-id');
 
-    updateStatus(action, tamuId);
+    //     updateStatus(action, tamuId);
 
-        location.reload();
+    //     location.reload();
+    // });
+
+    // function updateStatus(action, id) {
+    //     $.ajax({
+    //         url: '{{ url('updateStatus') }}/' + id,
+    //         method: 'POST',
+    //         data: {
+    //             _token: '{{ csrf_token() }}',
+    //             action: action
+    //         },
+    //         success: function(response) {
+    //             console.log(response);
+    //         },
+    //         error: function(xhr, status, error) {
+    //             console.log(error);
+    //         }
+    //     });
+    // }
+
+    // fungsi untuk mengubah status tamu
+    $(document).on('change', '.statusSwitch', function() {
+        var id = $(this).data('tamu-id');
+        var action = $(this).prop('checked') == true ? 'keluar' : 'aktif';
+        updateStatus(action, id);
     });
 
+    // fungsi untuk update status tamu
     function updateStatus(action, id) {
         $.ajax({
             url: '{{ url('updateStatus') }}/' + id,
-            method: 'POST',
+            type: 'POST',
             data: {
                 _token: '{{ csrf_token() }}',
-                action: action
+                action: action,
+                id: id
             },
             success: function(response) {
-                console.log(response);
+                $('#tableTamu').DataTable().ajax.reload();
             },
-            error: function(xhr, status, error) {
-                console.log(error);
+            error: function(xhr) {
+                alert('gagal mengubah status tamu');
             }
         });
     }
